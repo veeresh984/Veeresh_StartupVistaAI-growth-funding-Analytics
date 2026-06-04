350b187fb184a3dcfa97fea2074854e722d175c6
veeresh984 <veereshsbiradar984@gmail.com>
2026-06-04 11:54:12 +0530

    Update 02_Funding_Analytics.py

--- pages/02_Funding_Analytics.py
+++ pages/02_Funding_Analytics.py
@@ -1,1 +1,25 @@
+import streamlit as st
 
+from utils.data_loader import load_data
+from utils.charts import funding_histogram
+
+st.title("💰 Funding Analytics")
+
+df = load_data()
+
+fig = funding_histogram(df)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+top_funded = (
+    df.sort_values(
+        "Funding Amount (M USD)",
+        ascending=False
+    )
+    .head(20)
+)
+
+st.dataframe(top_funded)
