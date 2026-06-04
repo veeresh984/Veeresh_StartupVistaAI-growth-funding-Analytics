9e377cf3162fb8b5cee4b1164e6f1e46bdf6a108
veeresh984 <veereshsbiradar984@gmail.com>
2026-06-04 11:16:15 +0530

    Update 05_Startup_Scoring.py

--- pages/05_Startup_Scoring.py
+++ pages/05_Startup_Scoring.py
@@ -1,1 +1,37 @@
+import streamlit as st
+import pandas as pd
+from sklearn.cluster import KMeans
 
+df = pd.read_csv("data/startup_data.csv")
+
+features = df[
+[
+    "Funding Amount (M USD)",
+    "Revenue (M USD)",
+    "Valuation (M USD)"
+]
+]
+
+model = KMeans(
+    n_clusters=4,
+    random_state=42
+)
+
+df["Cluster"] = model.fit_predict(
+    features
+)
+
+st.title("Startup Segmentation")
+
+st.dataframe(df.head())
+
+st.plotly_chart(
+    px.scatter(
+        df,
+        x="Revenue (M USD)",
+        y="Valuation (M USD)",
+        color="Cluster",
+        hover_name="Startup Name"
+    ),
+    use_container_width=True
+)
